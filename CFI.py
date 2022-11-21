2879838c905457a2fbd24e78641aae2bf3179539
roroturgeon <118689456+roroturgeon@users.noreply.github.com>
2022-11-21 09:43:12 -0500

    Condition

---
+++ CFI.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 09:41:33 2022
+
+@author: Rosalie
+"""
+
+vhiuh
---
+++ CI.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 09:42:40 2022
+
+@author: Rosalie
+"""
+
