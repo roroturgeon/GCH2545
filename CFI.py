ce92e6e2b6e61c657a9c3c00ddd71a4ab8321b7f
roroturgeon <118689456+roroturgeon@users.noreply.github.com>
2022-11-28 09:53:26 -0500

    Automatisation conidtion robin

--- CFI.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 09:41:33 2022
-
-@author: Rosalie
-"""
-
--- CI.py
+++ CI.py
@@ -33,3 +33,31 @@ def CI(prm):
     Ti[:]=27.92 - 240.89*z[:] + 2982.72*z[:]**2   
     
     return Ti
+
+def CFI(prm):
+    """Fonction 
+    
+    Entrées:
+        - prm : Objet class parametres()
+            - Cp :Capacité thermique (J/K)
+            - K :Conductivié thermique (W/m*K)
+            - n : Nombre de points [-]
+            - rho : Masse volumique [kg/m^3]
+            - h : Coefficient de convection [W/m^2*K]
+            - H : Hauteur de la pâte [m]
+            - dz : Discrétisation en espace [m]
+            - dt : Discrétisation en temps [s]
+
+    
+    Sortie:
+        - Vecteur (array) composée de la température inférieure (z= 0 m) selon le nombre de points
+    """
+    dt=prm.dt
+    ti=prm.ti
+    tf=prm.tf
+    n=prm.n
+    t=np.linspace(ti,tf,int((tf-ti)/dt+1))
+    Tiinf=np.zeros(int((tf-ti)/dt+1))
+    Tiinf[:]=0.03175*t[:] + 26.015   
+    
+    return Tiinf
